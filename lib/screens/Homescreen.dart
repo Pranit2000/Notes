3c508caad3d58534541867c68e56fbd8dc738bf4
Pranit <khakal.pranit@gmail.com>
2022-10-09 17:13:23 +0530

    commit

--- lib/screens/Homescreen.dart
+++ lib/screens/Homescreen.dart
@@ -1,5 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:notetakingapp/style/appstyle.dart';
+import 'package:notetakingapp/widgets/note_card.dart';
 
 class Homescreen extends StatefulWidget {
   const Homescreen({Key? key}) : super(key: key);
@@ -28,8 +31,38 @@ class _HomescreenState extends State<Homescreen> {
             style: TextStyle(
                 fontSize: 22, fontWeight: FontWeight.bold, color: Colors.white),
           ),
-          
+          SizedBox(
+            height: 20,
+          ),
+          Expanded(
+            child: StreamBuilder<QuerySnapshot>(
+                stream:
+                    FirebaseFirestore.instance.collection("Notes").snapshots(),
+                builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+                  if (snapshot.hasData) {
+                    return GridView(
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2),
+                      children: snapshot.data!.docs
+                          .map((note) => noteCard(() {}, note))
+                          .toList(),
+                    );
+                  }
+                  return Text("There is no Notes",
+                      style: GoogleFonts.nunito(color: Colors.white));
+                }),
+          )
         ],
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {},
+        label: Text("Add Note"),
+        icon: Icon(Icons.add),
       ),
     );
   }
